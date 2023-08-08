8cad1a003261d614d1a45e032af19022784b383e
ahlam-laouadi <ahlamahlamitaa144@gmail.com>
2023-08-08 17:17:17 +0100

    first commit

--- middleware/testUser.js
+++ middleware/testUser.js
@@ -2,7 +2,7 @@ import { BadRequestError } from "../errors/index.js";
 
 const testUser = (req, res, next) => {
   if (req.user.testUser) {
-    throw new BadRequestError("Test User. Read Only!");
+    throw new BadRequestError("Test User Read Only!");
   }
   next();
 };
